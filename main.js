b37e95bef2a2694046172fbb815a421edab06408
CDS-admin1 <csouthon44@gmail.com>
2023-09-12 12:27:29 +0100

    Updating repo

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    // options object. can be left empty to get it started.
+        // creating this give us our view instance which powers our application
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Gloves for feet!'
+        }
+    }
+})
